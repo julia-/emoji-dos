f247974a3928e634e1e488394f613fa074cec483
julia- <julia.ryanx@gmail.com>
2017-11-28 12:11:53 +1100

    change to use data to render items

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
+const items = [
+  { description: 'First', completed: true },
+  { description: 'Second', completed: true },
+  { description: 'Third', completed: false },
+  { description: 'Fourth', completed: false }
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoItem description='First' completed />
-        <TodoItem description='Second' />
-        <TodoItem description='Third' />
+        {
+          items.map((item, index) => (
+            <TodoItem
+              key={ index }
+              description={ item.description }
+              completed={ item.completed }
+            />
+          ))
+        }
       </div>
     );
   }
